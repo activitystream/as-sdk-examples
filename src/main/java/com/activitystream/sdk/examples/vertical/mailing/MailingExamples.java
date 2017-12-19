ecf82367be92069134fe1d26f8f98b16af74c050
milica.vucic <milica.vucic@activitystream.com>
2017-12-19 13:45:11 +0100

    Code update

--- src/main/java/com/activitystream/sdk/examples/vertical/mailing/MailingExamples.java
+++ src/main/java/com/activitystream/sdk/examples/vertical/mailing/MailingExamples.java
@@ -1,192 +1,191 @@
 package com.activitystream.sdk.examples.vertical.mailing;
 
-import com.activitystream.core.model.aspects.ClassificationAspect;
-import com.activitystream.core.model.aspects.ClientIpAspect;
-import com.activitystream.core.model.aspects.ContentAspect;
-import com.activitystream.core.model.aspects.PresentationAspect;
-import com.activitystream.core.model.aspects.ResolvableAspect;
-import com.activitystream.core.model.aspects.TimedAspect;
-import com.activitystream.core.model.aspects.TrafficSource;
-import com.activitystream.core.model.aspects.TrafficSourceAspect;
 import com.activitystream.core.model.relations.ASEntityRelationTypes;
 import com.activitystream.core.model.relations.ASEventRelationTypes;
 import com.activitystream.sdk.ASEntity;
 import com.activitystream.sdk.ASEvent;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
+import static com.activitystream.core.model.aspects.ClassificationAspect.classification;
+import static com.activitystream.core.model.aspects.ClientIpAspect.clientIP;
+import static com.activitystream.core.model.aspects.ContentAspect.content;
+import static com.activitystream.core.model.aspects.PresentationAspect.presentation;
+import static com.activitystream.core.model.aspects.ResolvableAspect.resolvable;
+import static com.activitystream.core.model.aspects.TimedAspect.timed;
+import static com.activitystream.core.model.aspects.TrafficSource.trafficSource;
+import static com.activitystream.core.model.aspects.TrafficSourceAspect.trafficSources;
+
 public class MailingExamples {
 
     public static ASEvent createEmailSentEvent() throws JsonProcessingException {
-        ASEvent emailSentEvent = new ASEvent();
-        emailSentEvent.withType(ASEvent.PRE.AS_CRM_MESSAGE_EMAIL_SENT);
-        emailSentEvent.withOccurredAt("2017-02-05T21:37:09.000Z");
-        emailSentEvent.withOrigin("mail.system");
+        ASEvent emailSentEvent = new ASEvent()
+                .withType(ASEvent.PRE.AS_CRM_MESSAGE_EMAIL_SENT)
+                .withOccurredAt("2017-02-05T21:37:09.000Z")
+                .withOrigin("mail.system")
 
-        emailSentEvent.withRelationIfValid(ASEventRelationTypes.ACTOR, "MailCampaign", "852456");
-        emailSentEvent.withRelationIfValid(ASEventRelationTypes.INVOLVES, "MailSubscriber", "1159");
-        emailSentEvent.withRelationIfValid(ASEventRelationTypes.INVOLVES, "Email", "pera.detlic@gmail.com");
+                .withRelationIfValid(ASEventRelationTypes.ACTOR, "MailCampaign", "852456")
+                .withRelationIfValid(ASEventRelationTypes.INVOLVES, "MailSubscriber", "1159")
+                .withRelationIfValid(ASEventRelationTypes.INVOLVES, "Email", "pera.detlic@gmail.com")
 
-        emailSentEvent.withAspect(new ResolvableAspect().withExternalId("45834911"));
-        emailSentEvent.withAspect(new ClientIpAspect().withIp("127.0.0.1"));
+                .withAspect(resolvable().withExternalId("45834911"))
+                .withAspect(clientIP("127.0.0.1"));
 
         return emailSentEvent;
     }
 
     public static ASEvent createEmailClickedEvent() throws JsonProcessingException {
-        ASEvent emailClickedEvent = new ASEvent();
-        emailClickedEvent.withType(ASEvent.PRE.AS_CRM_MESSAGE_EMAIL_CLICKED);
-        emailClickedEvent.withOccurredAt("2017-12-05T21:37:09.000Z");
-        emailClickedEvent.withOrigin("mail.system");
+        ASEvent emailClickedEvent = new ASEvent()
+                .withType(ASEvent.PRE.AS_CRM_MESSAGE_EMAIL_CLICKED)
+                .withOccurredAt("2017-12-05T21:37:09.000Z")
+                .withOrigin("mail.system")
 
-        emailClickedEvent.withRelationIfValid(ASEventRelationTypes.ACTOR, "Email", "pera.detlic@gmail.com");
-        //        emailClickedEvent.withRelationIfValid(ASEventRelationTypes.ACTOR, "MailSubscriber", "1159"); TODO Option for Actor
-        emailClickedEvent.withRelationIfValid(ASEventRelationTypes.INVOLVES, "MailCampaign", "852456");
-        emailClickedEvent.withRelationIfValid(ASEventRelationTypes.REFERENCES, "MarketingURL", "719181145");
+                .withRelationIfValid(ASEventRelationTypes.ACTOR, "Email", "pera.detlic@gmail.com")
+                .withRelationIfValid(ASEventRelationTypes.INVOLVES, "MailCampaign", "852456")
+                .withRelationIfValid(ASEventRelationTypes.REFERENCES, "MarketingURL", "719181145")
 
-        emailClickedEvent.withAspect(new ResolvableAspect().withExternalId("10460811"));
-        emailClickedEvent.withAspect(new TrafficSourceAspect().addTrafficSource(new TrafficSource().withType("campaign").withCampaign("MailCampaign/852456").withMedium("mail")));
-        emailClickedEvent.withAspect(new ClientIpAspect().withIp("127.0.0.1"));
+                .withAspect(resolvable().withExternalId("10460811"))
+                .withAspect(trafficSources().addTrafficSource(trafficSource().withType("campaign").withCampaign("MailCampaign/852456").withMedium("mail")))
+                .withAspect(clientIP("127.0.0.1"));
 
         return emailClickedEvent;
     }
 
     public static ASEvent createEmailOpenedEvent() throws JsonProcessingException {
-        ASEvent emailOpenedEvent = new ASEvent();
-        emailOpenedEvent.withType(ASEvent.PRE.AS_CRM_MESSAGE_EMAIL_OPENED);
-        emailOpenedEvent.withOccurredAt("2017-01-05T21:37:09.000Z");
-        emailOpenedEvent.withOrigin("mail.system");
+        ASEvent emailOpenedEvent = new ASEvent()
+                .withType(ASEvent.PRE.AS_CRM_MESSAGE_EMAIL_OPENED)
+                .withOccurredAt("2017-01-05T21:37:09.000Z")
+                .withOrigin("mail.system")
 
-        emailOpenedEvent.withRelationIfValid(ASEventRelationTypes.ACTOR, "Email", "pera.detlic@gmail.com");
-        //        emailOpenedEvent.withRelationIfValid(ASEventRelationTypes.ACTOR, "MailSubscriber", "1159"); TODO Option for Actor
-        emailOpenedEvent.withRelationIfValid(ASEventRelationTypes.INVOLVES, "MailCampaign", "852456");
+                .withRelationIfValid(ASEventRelationTypes.ACTOR, "Email", "pera.detlic@gmail.com")
+                .withRelationIfValid(ASEventRelationTypes.INVOLVES, "MailCampaign", "852456")
 
-        emailOpenedEvent.withAspect(new ResolvableAspect().withExternalId("45834911"));
-        emailOpenedEvent.withAspect(new TrafficSourceAspect().addTrafficSource(new TrafficSource().withType("campaign").withCampaign("MailCampaign/852456").withMedium("mail")));
-        emailOpenedEvent.withAspect(new ClientIpAspect().withIp("127.0.0.1"));
+                .withAspect(resolvable().withExternalId("45834911"))
+                .withAspect(trafficSources().addTrafficSource(trafficSource().withType("campaign").withCampaign("MailCampaign/852456").withMedium("mail")))
+                .withAspect(clientIP("127.0.0.1"));
 
         return emailOpenedEvent;
     }
 
     public static ASEvent createEmailBouncedEvent() throws JsonProcessingException {
-        ASEvent emailBouncedEvent = new ASEvent();
-        emailBouncedEvent.withType(ASEvent.PRE.AS_CRM_MESSAGE_EMAIL_BOUNCED);
-        emailBouncedEvent.withOccurredAt("2017-10-05T21:37:09.000Z");
-        emailBouncedEvent.withOrigin("mail.system");
+        ASEvent emailBouncedEvent = new ASEvent()
+                .withType(ASEvent.PRE.AS_CRM_MESSAGE_EMAIL_BOUNCED)
+                .withOccurredAt("2017-10-05T21:37:09.000Z")
+                .withOrigin("mail.system")
 
-        emailBouncedEvent.withRelationIfValid(ASEventRelationTypes.ACTOR, "Email", "pera.detlic@gmail.com");
-        emailBouncedEvent.withRelationIfValid(ASEventRelationTypes.AFFECTS, "MailCampaign", "852456");
+                .withRelationIfValid(ASEventRelationTypes.ACTOR, "Email", "pera.detlic@gmail.com")
+                .withRelationIfValid(ASEventRelationTypes.AFFECTS, "MailCampaign", "852456")
 
-        emailBouncedEvent.withAspect(new ClassificationAspect().withType("bounce").withVariant("soft"));
-        emailBouncedEvent.withAspect(new ResolvableAspect().withExternalId("45834911"));
-        emailBouncedEvent.withAspect(new TrafficSourceAspect().addTrafficSource(new TrafficSource().withType("campaign").withCampaign("MailCampaign/852456").withMedium("mail")));
-        emailBouncedEvent.withAspect(new ClientIpAspect().withIp("127.0.0.1"));
+                .withAspect(classification().withType("bounce").withVariant("soft"))
+                .withAspect(resolvable().withExternalId("45834911"))
+                .withAspect(trafficSources().addTrafficSource(trafficSource().withType("campaign").withCampaign("MailCampaign/852456").withMedium("mail")))
+                .withAspect(clientIP("127.0.0.1"));
 
         return emailBouncedEvent;
     }
 
     public static ASEvent createEmailSubscribedEvent() throws JsonProcessingException {
-        ASEvent emailSubscribedEvent = new ASEvent();
-        emailSubscribedEvent.withType(ASEvent.PRE.AS_CRM_EMAIL_SUBSCRIBED);
-        emailSubscribedEvent.withOccurredAt("2017-03-05T21:37:09.000Z");
-        emailSubscribedEvent.withOrigin("mail.system");
+        ASEvent emailSubscribedEvent = new ASEvent()
+                .withType(ASEvent.PRE.AS_CRM_EMAIL_SUBSCRIBED)
+                .withOccurredAt("2017-03-05T21:37:09.000Z")
+                .withOrigin("mail.system")
 
-        emailSubscribedEvent.withDimension("list_optin", "true");
+                .withDimension("list_optin", "true")
 
-        emailSubscribedEvent.withRelationIfValid(ASEventRelationTypes.ACTOR, "Email", "pera.detlic@gmail.com");
-        emailSubscribedEvent.withRelationIfValid(ASEventRelationTypes.AFFECTS, "MailingList", "2");
+                .withRelationIfValid(ASEventRelationTypes.ACTOR, "Email", "pera.detlic@gmail.com")
+                .withRelationIfValid(ASEventRelationTypes.AFFECTS, "MailingList", "2")
 
-        emailSubscribedEvent.withAspect(new ResolvableAspect().withExternalId("45834911"));
-        emailSubscribedEvent.withAspect(new ClientIpAspect().withIp("127.0.0.1"));
+                .withAspect(resolvable().withExternalId("45834911"))
+                .withAspect(clientIP("127.0.0.1"));
 
         return emailSubscribedEvent;
     }
 
     public static ASEvent createEmailUnsubscribedEvent() throws JsonProcessingException {
-        ASEvent emailUnsubscribedEvent = new ASEvent();
-        emailUnsubscribedEvent.withType(ASEvent.PRE.AS_CRM_EMAIL_UNSUBSCRIBED);
-        emailUnsubscribedEvent.withOccurredAt("2017-11-05T21:37:09.000Z");
-        emailUnsubscribedEvent.withOrigin("mail.system");
+        ASEvent emailUnsubscribedEvent = new ASEvent()
+                .withType(ASEvent.PRE.AS_CRM_EMAIL_UNSUBSCRIBED)
+                .withOccurredAt("2017-11-05T21:37:09.000Z")
+                .withOrigin("mail.system")
 
-        emailUnsubscribedEvent.withDimension("list_optin", "true");
+                .withDimension("list_optin", "true")
 
-        emailUnsubscribedEvent.withRelationIfValid(ASEventRelationTypes.ACTOR, "Email", "pera.detlic@gmail.com");
-        emailUnsubscribedEvent.withRelationIfValid(ASEventRelationTypes.AFFECTS, "MailingList", "2");
+                .withRelationIfValid(ASEventRelationTypes.ACTOR, "Email", "pera.detlic@gmail.com")
+                .withRelationIfValid(ASEventRelationTypes.AFFECTS, "MailingList", "2")
 
-        emailUnsubscribedEvent.withAspect(new ResolvableAspect().withExternalId("45834911"));
-        emailUnsubscribedEvent.withAspect(new ClientIpAspect().withIp("127.0.0.1"));
+                .withAspect(resolvable().withExternalId("45834911"))
+                .withAspect(clientIP("127.0.0.1"));
 
         return emailUnsubscribedEvent;
     }
 
-    public static ASEntity createSubscriberEntity() throws JsonProcessingException { //TODO Check where to add it
-        ASEntity subscriber = new ASEntity("MailSubscriber", "1159");
+    public static ASEntity createSubscriberEntity() throws JsonProcessingException {
+        ASEntity subscriber = new ASEntity("MailSubscriber", "1159")
 
-        subscriber.withDimension("status", "subscribed");
-        subscriber.withDimension("status_code", "1");
+                .withDimension("status", "subscribed")
+                .withDimension("status_code", "1")
 
-        subscriber.withRelationIfValid(ASEntityRelationTypes.AKA, "Email", "pera.detlic@gmail.com");
-        subscriber.withRelationIfValid(ASEntityRelationTypes.AKA, "Email", "petar.pera.detlic@gmail.com");
+                .withRelationIfValid(ASEntityRelationTypes.AKA, "Email", "pera.detlic@gmail.com")
+                .withRelationIfValid(ASEntityRelationTypes.AKA, "Email", "petar.pera.detlic@gmail.com")
 
-        subscriber.withAspect(new PresentationAspect("Petar Pera Detlic"));
-        subscriber.withAspect(new ClassificationAspect("active"));
-        subscriber.withAspect(new TimedAspect().withPeriod("subscribed_on", "2017-07-23T20:30:09.000Z"));
-        subscriber.withAspect(new ClientIpAspect().withIp("127.0.0.1"));
+                .withAspect(presentation().withLabel("Petar Pera Detlic"))
+                .withAspect(classification().withType("active"))
+                .withAspect(timed().withPeriod("subscribed_on", "2017-07-23T20:30:09.000Z"))
+                .withAspect(clientIP("127.0.0.1"));
 
         return subscriber;
     }
 
     public static ASEntity createEmailEntity() throws JsonProcessingException {
-        ASEntity email = new ASEntity("Email", "pera.detlic@gmail.com");
+        ASEntity email = new ASEntity("Email", "pera.detlic@gmail.com")
 
-        email.withDimension("email_service", "gmail");
-        email.withDimension("status", "valid");
+                .withDimension("email_service", "gmail")
+                .withDimension("status", "valid")
 
-        email.withRelationIfValid(ASEntityRelationTypes.PART_OF, "MailingList", "2");
+                .withRelationIfValid(ASEntityRelationTypes.PART_OF, "MailingList", "2")
 
-        email.withAspect(new PresentationAspect().withLabel("Petar Pera Detlic"));
-        email.withAspect(new ClassificationAspect().withType("private"));
+                .withAspect(presentation().withLabel("Petar Pera Detlic"))
+                .withAspect(classification().withType("private"));
 
         return email;
     }
 
     public static ASEntity createCampaignEntity() throws JsonProcessingException {
-        ASEntity mailCampaign = new ASEntity("MailCampaign", "852456");
+        ASEntity mailCampaign = new ASEntity("MailCampaign", "852456")
 
-        mailCampaign.withRelationIfValid(ASEntityRelationTypes.AKA, "MailGoogleAnalyticsName", "glnm");
-        mailCampaign.withRelationIfValid(ASEntityRelationTypes.BELONGS_TO, "MailingList", "2");
-        mailCampaign.withRelationIfValid(ASEntityRelationTypes.BELONGS_TO, "MailingList", "36");
-        mailCampaign.withRelationIfValid(ASEntityRelationTypes.PART_OF, "MailClient", "1093");
+                .withDimension("content_type", "standard")
+                .withDimension("is_test", "false")
+                .withDimension("track_replies", "true")
+                .withDimension("reply_to", "info@w3schools.com")
 
-        mailCampaign.withAspect(new ContentAspect().withTitle("W3Schools ").withSubtitle("Tutorials").withContent("<!DOCTYPE html> <html> <body> <img src=\"w3schools.jpg\" alt=\"W3Schools.com\" width=\"104\" height=\"142\"> </body> </html>"));
-        mailCampaign.withAspect(new ClassificationAspect().withType("campaign").withVariant("standard"));
-        mailCampaign.withAspect(new PresentationAspect().withLabel("Learning with W3Schools"));
+                .withRelationIfValid(ASEntityRelationTypes.AKA, "MailGoogleAnalyticsName", "glnm")
+                .withRelationIfValid(ASEntityRelationTypes.BELONGS_TO, "MailingList", "2")
+                .withRelationIfValid(ASEntityRelationTypes.BELONGS_TO, "MailingList", "36")
+                .withRelationIfValid(ASEntityRelationTypes.PART_OF, "MailClient", "1093")
 
-        mailCampaign.withDimension("content_type", "standard");
-        mailCampaign.withDimension("is_test", "false");
-        mailCampaign.withDimension("track_replies", "true");
-        mailCampaign.withDimension("reply_to", "info@w3schools.com");
+                .withAspect(content().withTitle("W3Schools ").withSubtitle("Tutorials").withContent("<!DOCTYPE html> <html> <body> <img src=\"w3schools.jpg\" alt=\"W3Schools.com\" width=\"104\" height=\"142\"> </body> </html>"))
+                .withAspect(classification().withType("campaign").withVariant("standard"))
+                .withAspect(presentation().withLabel("Learning with W3Schools"))
 
-        mailCampaign.withProperties("status", "complete");
-        mailCampaign.withProperties("authenticate", "false");
-        mailCampaign.withProperties("show_in_archive", "false");
-        mailCampaign.withProperties("view_in_browser", "false");
+                .withProperties("status", "complete")
+                .withProperties("authenticate", "false")
+                .withProperties("show_in_archive", "false")
+                .withProperties("view_in_browser", "false");
 
         return mailCampaign;
     }
 
     public static ASEntity createMailingListEntity() throws JsonProcessingException {
-        ASEntity mailingList = new ASEntity("MailingList", "2");
+        ASEntity mailingList = new ASEntity("MailingList", "2")
 
-        mailingList.withAspect(new ClassificationAspect().withType("public"));
-        mailingList.withAspect(new PresentationAspect().withLabel("CSS Website Templates Newslatters"));
+                .withAspect(classification().withType("public"))
+                .withAspect(presentation().withLabel("CSS Website Templates Newslatters"));
 
         return mailingList;
     }
 
     public static ASEntity createURLEntity() throws JsonProcessingException {
-        ASEntity url = new ASEntity("MarketingURL", "714683");
+        ASEntity url = new ASEntity("MarketingURL", "714683")
 
-        url.withAspect(new PresentationAspect().withLabel("W3.CSS Website Templates").withDetailsUrl("https://www.w3schools.com/w3css/w3css_templates.asp"));
+                .withAspect(presentation().withLabel("W3.CSS Website Templates").withDetailsUrl("https://www.w3schools.com/w3css/w3css_templates.asp"));
 
         return url;
     }
